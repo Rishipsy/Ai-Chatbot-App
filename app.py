e707971c667726d09df8c5b47bb5370ce6d4cf69
Rishabh <riship2410@gmail.com>
2026-08-10 10:42:56 +0530

    Phase 3: basic flask serfer

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-# Flask app entry point
+from os import environ
+
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    port = int(environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
